d338721650c47947c18e2f2d7be156389644f702
EJEZIE OLISAEMEKA <olisaejezie@gmail.com>
2025-09-14 17:24:05 +0100

    refactored my code by creating a routeToController function

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
 
 require "functions.php";
-
+//passing values to uri
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
+//declaring my routes
 $routes = [
     '/vanillaphp/index' => "controllers/index.php",
     '/vanillaphp/index.php/about'=> "controllers/about.php",
     '/vanillaphp/index.php/contact'=> "controllers/contact.php"
 ];
-
+//a function to handle routing
+function routeToController($uri, $routes){
+    if(array_key_exists($uri, $routes)){
+    require $routes[$uri];
+    } else{
+    abort();
+    }
+}
+//function to abort with the given status code
 function abort($code = 404){
     http_response_code($code);
 
@@ -18,10 +26,6 @@ function abort($code = 404){
     die();
 }
 
-if(array_key_exists($uri, $routes)){
-    require $routes[$uri];
-} else{
-    abort();
-}
+routeToController($uri, $routes);
 
 ?>
