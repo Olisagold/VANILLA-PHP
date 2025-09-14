17a713d20181ca88ce9f4c884e2abb34af07665d
EJEZIE OLISAEMEKA <olisaejezie@gmail.com>
2025-09-14 10:46:45 +0100

    wrote a code to throw error message as a continuation of the former if statement and created a 404.php file under the views directory and required it from index.php

--- index.php
+++ index.php
@@ -4,15 +4,6 @@ require "functions.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-
-// if($uri === '/vanillaphp/index'){
-//     require "controllers/index.php";
-// } else if($uri === '/vanillaphp/index.php/about') {
-//     require "controllers/about.php";
-//  } else if($uri === "/vanillaphp/index.php/contact") {
-//     require "controllers/contact.php";
-//  }
-
 $routes = [
     '/vanillaphp/index' => "controllers/index.php",
     '/vanillaphp/index.php/about'=> "controllers/about.php",
@@ -21,5 +12,12 @@ $routes = [
 
 if(array_key_exists($uri, $routes)){
     require $routes[$uri];
+} else{
+    http_response_code(404);
+
+    require "views/404.php";
+
+    die();
 }
+
 ?>
---
+++ views/404.php
@@ -0,0 +1,18 @@
+<?php 
+  require('partials/head.php');
+  require('partials/nav.php');
+
+?>
+
+ 
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <h1 class="text-2xl font-bold"> Sorry, Page  Not Found</h1>
+    <p>
+        <a href="/vanillaphp/index" class="text-blue underline">Go back to home</a>
+    </p>
+    
+    </div>
+  </main>
+
+  <?php require('partials/footer.php') ?>
