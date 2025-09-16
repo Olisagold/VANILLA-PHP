744e4d7467d4752df3d8bfeed4671d9fd6885150
EJEZIE OLISAEMEKA <olisaejezie@gmail.com>
2025-09-16 03:07:34 +0100

    refactoring the connection of database and execution of a query into a class

--- index.php
+++ index.php
@@ -3,19 +3,27 @@
 require "functions.php";
 // require "router.php";
 
+//connect to the database and execute a query.
 
-// connect to mysql database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
+class Database{
 
-//query to send to mysql and execution
-$statement = $pdo->prepare("SELECT * FROM `posts`");
-$statement->execute();
+    public function query(){
+        // connect to mysql database
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        $pdo = new PDO($dsn);
 
-//fetched all results
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        //query to send to mysql and execution
+        $statement = $pdo->prepare("SELECT * FROM `posts`");
+        $statement->execute();
 
-// dd($posts);
+        //fetched all results
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+//object
+$db = new Database();
+
+$posts = $db->query();
 
 //displayed the data in the database as an unordered list
 foreach($posts as $post){
