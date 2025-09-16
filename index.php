8e29758b13a1f19283290981209593207849ccea
EJEZIE OLISAEMEKA <olisaejezie@gmail.com>
2025-09-16 02:13:35 +0100

    ïnroduction to classes with respect to connecting to a database

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
 ?>
+
+
+// connect to mysql database
+
+class Person{
+    public $name;
+    public $age;
+
+     public function breathe(){
+        echo "breathing";
+    }
+}
+
+$person = new Person();
+
+$person->name = "John Doe";
+$person->age = "25"
+
+dd($person->age);
