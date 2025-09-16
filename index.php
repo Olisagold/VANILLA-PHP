6be74a698652aaee368523858c2e0cf4e7c77198
EJEZIE OLISAEMEKA <olisaejezie@gmail.com>
2025-09-16 03:51:26 +0100

    refactored the dataabse connection block of code again this time with a construct function

--- index.php
+++ index.php
@@ -6,27 +6,33 @@ require "functions.php";
 //connect to the database and execute a query.
 
 class Database{
+    public $connection;
+
+    public function __construct(){
+         // connect to mysql database
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+        $this->connection = new PDO($dsn);
+    }
 
     public function query($query){
-        // connect to mysql database
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-        $pdo = new PDO($dsn);
-
         //query to send to mysql and execution
-        $statement = $pdo->prepare($query);
+        $statement = $this->connection->prepare($query);
         $statement->execute();
 
         //fetched all results
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $statement;
     }
 }
 //object
 $db = new Database();
 
-$posts = $db->query("SELECT * FROM `posts` where id = 1;");
+$posts = $db->query("SELECT * FROM `posts`")->fetchAll(PDO::FETCH_ASSOC);;
+
+dd($posts);
 
 //displayed the data in the database as an unordered list
-foreach($posts as $post){
-    echo "<li>".$post['title']."</li>";
-}
+// foreach($posts as $post){
+//     echo "<li>".$post['title']."</li>";
+// }
 ?>
